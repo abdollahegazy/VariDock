25dda3f412fb616bf568caf827d93fe8cfee4563
abdollahegazy <abdolla.hegazy2006@gmail.com>
2026-02-07 21:33:44 -0500

    slurm counting

--- varidock/execution/__init__.py
+++ varidock/execution/__init__.py
@@ -2,6 +2,7 @@ from .materialize import PlanMaterializer, DefaultMaterializer
 from .run import CommandRunner, LocalCommandRunner, CompletedRun
 from .validate import PlanValidator, ExpectedOutputsValidator
 from .local import LocalExecutor
+from .slurm import _sbatch, get_slurm_queue_count
 
 __all__ = [
     "PlanMaterializer",
@@ -12,4 +13,6 @@ __all__ = [
     "PlanValidator",
     "ExpectedOutputsValidator",
     "LocalExecutor",
+    "_sbatch",
+    "get_slurm_queue_count",
 ]
